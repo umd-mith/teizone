52754f3e6d4bcdb7f1b63010f571d659b5f14c09
Ed Summers <ehs@pobox.com>
2014-12-05 11:50:59 -0500

    new release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class PyTest(Command):
 
 setup(
     name = 'teizone',
-    version = '0.0.1',
+    version = '0.0.2',
     url = 'http://github.com/umd_mith/teizone',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
